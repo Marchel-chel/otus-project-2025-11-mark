8998df73eba8a370f952a8892a6115b0a3b462f6
Marchel <mark.lu@simpleswap.io>
2025-11-11 22:49:14 +0200

    Fix allure-reporter and add scripts for PW and Allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig, devices } from "@playwright/test";
-import * as dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 
 export default defineConfig({
   testDir: "./tests",
   timeout: 30_000,
   expect: { timeout: 5_000 },
-  reporter: [["html", { outputFolder: "playwright-report" }]],
+  reporter: [
+    ["line"],
+    [
+      "allure-playwright",
+      { outputFolder: "allure-results", detail: true, suiteTitle: false },
+    ],
+  ],
   use: {
     baseURL: process.env.BASE_URL || "https://demowebshop.tricentis.com",
     trace: "retain-on-failure",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
     headless: false,
   },
   projects: [
